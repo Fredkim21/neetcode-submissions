960e997960d1c095433cfea709fee27543d53f9f
Fred Kim <104876440+Fredkim21@users.noreply.github.com>
2026-05-20 20:05:07 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const pairs = { '}' : '{' , ']' : '[' , ')' : '(' };
+        const stack = [];
+
+        for (const ch of s) {
+            if (ch === '(' || ch === '[' || ch === '{') {
+                stack.push(ch);
+            } else {
+                if (stack.length === 0 || stack.pop() !== pairs[ch]) {
+                    return false;
+                }
+            }
+        }
+        return stack.length === 0;
+    }
+}
